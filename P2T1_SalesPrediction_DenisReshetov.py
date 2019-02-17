92e33d128c96edf0ff0a32fc75b551c38496b798
reshetod3949 <47707180+reshetod3949@users.noreply.github.com>
2019-02-17 00:18:45 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_DenisReshetov.py
@@ -0,0 +1,21 @@
+# Learning how to use python.
+# 2/13/19
+# CTI-110 P2T1 - Sales Prediction
+# Denis Reshetov
+
+2. # Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+
+
+
+
+
+
+
